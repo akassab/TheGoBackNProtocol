0eb60547de8e197305a499519d0f0f58b66ee7c8
Ayman Kassab <aymkas@gmail.com>
2022-03-14 18:35:34 -0400

    Add program

---
+++ A2Sender/Program.cs
@@ -0,0 +1,32 @@
+﻿using A2Sender.services;
+
+namespace A2Sender
+{
+internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Program started..."); 
+
+            // verify/parse command line arguments.
+            if (ConsoleParametersService.TryParseAndSetArgs(args))
+            {
+                // start new thread to listen for acks indefinitely
+                ListenerService.ListenForAck();
+                
+                // try to send packets
+                if (SenderService.TrySendPackets()) {
+                    Console.WriteLine("Sent all packets!");
+                }
+                else {
+                    //  todo:  IF THIS FAILS WE SHOULD CANCEL THE THREAD ****************
+                    Console.WriteLine("Failed to send packets.");
+                }
+
+            }
+            else {
+                Console.WriteLine("Failed to parse console paramater(s).");
+            }
+        }
+    }
+}
